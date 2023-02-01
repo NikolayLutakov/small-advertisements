b39656a7bd8ca1700cefaa1548ec1543cb92a187
DESKTOP-KE37OG2\Nikolay <nikolaylutakov@gmail.com>
2023-02-01 12:52:46 +0200

    Add sqlite data and identity

---
+++ SmallAdvertisements/SmallAdvertisements/Data/Context/AdvertisementsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmallAdvertisements.Data.Entities;
+
+namespace SmallAdvertisements.Data.Context
+{
+    public class AdvertisementsDbContext : IdentityDbContext<User>
+    {
+        public AdvertisementsDbContext(DbContextOptions<AdvertisementsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- SmallAdvertisements/SmallAdvertisements/Program.cs
+++ SmallAdvertisements/SmallAdvertisements/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SmallAdvertisements.Data.Context;
+using SmallAdvertisements.Data.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AdvertisementsDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Db"));
+});
+
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AdvertisementsDbContext>();
 
 var app = builder.Build();
 
@@ -17,7 +36,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
