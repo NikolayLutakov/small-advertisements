34871cd5534c40f1174458b6af9ef944325ac1d8
DESKTOP-KE37OG2\Nikolay <nikolaylutakov@gmail.com>
2023-02-01 14:18:48 +0200

    Add login, register and logout

--- SmallAdvertisements/SmallAdvertisements/Data/Migrations/20230201112551_Initial.cs
+++ SmallAdvertisements/SmallAdvertisements/Data/Migrations/20230201112551_Initial.cs
@@ -30,7 +30,6 @@ namespace SmallAdvertisements.Data.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "TEXT", nullable: false),
-                    FullName = table.Column<string>(type: "TEXT", nullable: false),
                     UserName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
